ce064f92804ec7e0e0de4eb2898886ea8a171032
Gracjan Baryła <barylagracjan@outlook.com>
2022-11-23 12:58:28 +0000

    Version 1.0.1
    Code has been cleaned up a little bit and added under a namespace
    V1.0.1

--- Price Optimization/EconomyAI.cs
+++ Price Optimization/EconomyAI.cs
@@ -3,82 +3,87 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EconomyAI : MonoBehaviour
+namespace Ceriumin
 {
-    [Header("Price Statistics")]
-    [Tooltip("Set your desired price")]   
-    public float originalPrice;
-    [Tooltip("The price it is adjusted from")]   
-    public float beforeAdjustment;
-    [Tooltip("The adjusted price after inflation")]  
-    public float adjustedPrice;
-    public string inflationRate;
-    [Space(5)]
-
-    [Header("Independent Variables")]
-    [Range(0, 100)]
-    public int demand;
-    [Range(0, 100)]
-    public int supply;
-    [Space(5)]
-
-    [Header("Debug"), Tooltip("Weight affects how strong the price change is")]
-    public float weight;
-    public float priceDelay;
-    private float setDelay;
-    public bool setPrice;
-    public float supplyRate = 0f;
-    public float demandRate = 0f;
-
-    void Update()
+    public class EconomyAI : MonoBehaviour
     {
-        CalculatePrice();
-        Debugging();
-        CallDelay();
-    }
+        [Header("Price Statistics")]
+        [Tooltip("Set your desired price"), SerializedField]   
+        private  float originalPrice;
+        [Tooltip("The price it is adjusted from"), SerializedField]   
+        private  float beforeAdjustment;
+        [Tooltip("The adjusted price after inflation"), SerializedField]  
+        private  float adjustedPrice;
+        [SerializedField]    
+        private  string inflationRate;
+        [Space(5)]
 
-    void Start()
-    {
-        beforeAdjustment = originalPrice;
-        setDelay = priceDelay;
-    }
+        [Header("Independent Variables")]
+        [Range(0, 100), SerializedField]
+        private  int demand;
+        [Range(0, 100), SerializedField]
+        private  int supply;
+        [Space(5)]
 
-    void CalculatePrice()
-    {
-        //Setting the Adjusted Equillibrium price
-        demandRate = demand / weight;
-        supplyRate = supply / weight;
-        adjustedPrice = beforeAdjustment * (1f + -supplyRate + demandRate);
-        adjustedPrice = Mathf.Round(adjustedPrice * 100f) / 100.0f;
+        [Header("Debug"), Tooltip("Weight affects how strong the price change is")]
+        [SerializedField]
+        private  float weight;
+        [SerializedField]
+        private  float priceDelay;
+        [SerializedField]
+        private  bool setPrice;
 
-        //Calculating Inflation Rate
-        float inflate = ((adjustedPrice - originalPrice) / originalPrice) * 100;
-        inflationRate = (((int)inflate).ToString() + "%");
-    }
+        private float setDelay;
+        private  float supplyRate = 0f;
+        private  float demandRate = 0f;
 
-    void CallDelay()
-    {
-        //Setting new price after specified delay
-        setDelay -= Time.deltaTime;
-        if(setDelay < 0 && setPrice)
+        void Update()
         {
-            Debug.Log("Price has been Changed");
-            beforeAdjustment = adjustedPrice;
+            CalculatePrice();
+            CallDelay();
+        }
+
+        void Start()
+        {
+            beforeAdjustment = originalPrice;
             setDelay = priceDelay;
         }
 
-
-    }
-
-    void Debugging()
-    {
-        if(adjustedPrice < 0)
-            adjustedPrice = 0;
-        if(originalPrice < 0)
+        void CalculatePrice()
         {
-            originalPrice = 0;
-            Debug.Log("Set a price equal to or higher than 0");
+            //Setting the Adjusted Equillibrium price
+
+            demandRate = demand / weight;
+            supplyRate = supply / weight;
+            adjustedPrice = beforeAdjustment * (1f + -supplyRate + demandRate);
+            adjustedPrice = Mathf.Round(adjustedPrice * 100f) / 100.0f;
+
+            //Calculating Inflation Rate
+
+            var inflate = ((adjustedPrice - originalPrice) / originalPrice) * 100;
+            inflationRate = (int)inflate).ToString() + "%";
+
+            //Adjust price to 0 if it is negative
+            
+            if(adjustedPrice < 0)
+                adjustedPrice = 0;
+
+            if(originalPrice < 0)
+                originalPrice = 0;
+        }
+
+        void CallDelay()
+        {
+            //Setting new price after specified delay
+            
+            setDelay -= Time.deltaTime;
+            if(setDelay < 0 && setPrice)
+            {
+                beforeAdjustment = adjustedPrice;
+                setDelay = priceDelay;
+            }
+
+
         }
     }
 }
-
